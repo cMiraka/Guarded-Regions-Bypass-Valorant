486fed1a3fd11146fca7eb6ebaa398fc836490fb
Chase <70964202+Chase1803@users.noreply.github.com>
2022-08-17 09:52:28 +0530

    Update driver.h

--- User/driver.h
+++ User/driver.h
@@ -19,7 +19,7 @@ private:
 	};
 
 public:
-	auto initdriver(int processid) -> void
+	auto initdriver( int processid ) -> void
 	{
 		NtUserGetPointerProprietaryId = ( Nt_UserGetPointerProprietaryId )GetProcAddress( LoadLibraryA( "win32u.dll" ), "NtUserGetPointerProprietaryId" );
 		if ( NtUserGetPointerProprietaryId != 0 )
@@ -32,7 +32,7 @@ public:
 	auto readvm( uint32_t src_pid, uint64_t src_addr, uint64_t dst_addr, size_t size ) -> void
 	{
 		_requests out = { src_pid, src_addr, dst_addr, size, DRIVER_READVM };
-		NtUserGetPointerProprietaryId(reinterpret_cast<uintptr_t>(&out));
+		NtUserGetPointerProprietaryId( reinterpret_cast< uintptr_t >( &out ) );
 	}
 
 	auto guarded_region() -> uintptr_t
@@ -44,7 +44,7 @@ public:
 	}
 
 	template <typename T>
-	T readguarded(uint64_t guardedregion, uintptr_t src, size_t size = sizeof(T))
+	T readguarded( uint64_t guardedregion, uintptr_t src, size_t size = sizeof(T) )
 	{
 		T buffer;
 		readvm( _processid, src, ( uintptr_t )&buffer, size );
